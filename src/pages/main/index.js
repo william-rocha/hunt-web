c91b58349a998ab3a19422173c472d387e892035
william <williamrochajr@gmail.com>
2020-08-16 13:48:59 -0300

    armazenando no estado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 const Main = () => {
+    const [products, setProducts] = useState([])
     useEffect(() => {
         loadProducts()
     })
 
     const loadProducts = async () => {
         const response = await api.get(`/products`)
+        setProducts(response.data.docs)
         console.log('response', response);
     }
-    return ( <><div>hello rocketseat</div></> );
+return ( <><div>{products.map(product => (<h2 key={products._id}>{product.title}</h2>))}</div></> );
 }
  
 export default Main;

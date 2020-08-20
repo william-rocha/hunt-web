fcb76e6609df084ac64d20113219a597e9828ab0
william <williamrochajr@gmail.com>
2020-08-20 19:02:45 -0300

    Navegando para o detalhe

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import "./styles.css";
 
-const Product = () => {
+const Product = (props) => {
+    const [product, setProduct] = useState({});
+    useEffect(() => {
+        findProduct();
+    },[])
+
+    const findProduct = async () => {
+        const { id } = props.match.params
+        const response = await api.get(`/products/${id}`)
+        setProduct(response.data)
+    }
     return ( <>
-    <div>product</div>
+    	<div className="product-info">
+			<h1>{product.title}</h1>
+			<p>{product.description}</p>
+
+			<p>
+				URL: <a href={product.url}>{product.url}</a>
+			</p>
+		</div>
     </> );
 }
  
